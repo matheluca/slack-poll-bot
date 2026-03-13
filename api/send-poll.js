f3d18e6a802ba7845fa1c0852306edea0e4b5b78
matheluca <matheusluca11@gmail.com>
2026-03-12 23:36:05 -0300

    Update send-poll.js

--- api/send-poll.js
+++ api/send-poll.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         blocks: [
           {
             type: "section",
-            text: { type: "mrkdwn", text: `*${question}*` },
+            text: { type: "mrkdwn", text: `<!channel> *${question}*` },
           },
           {
             type: "actions",
